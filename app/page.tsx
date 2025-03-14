67e569d21e55fdf59cbea974a2d0a78e445f4265
Apostolos Karamanis <apostkaram@gmail.com>
2025-03-14 20:48:56 +0200

    added timeline, needs work

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import AnimatedMenu from "@/components/animated-menu/animated-menu";
+import Timeline from "@/components/timeline/timeline";
 
 export default function Home() {
   return (
     <div>
       <AnimatedMenu />
+      <Timeline />
     </div>
   );
 }
